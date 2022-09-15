af2f1f0b184ca2f7c72f59c1e1eb21cd5b268667
R1daS <rida.siddique@student.tdsb.on.ca>
2022-09-15 11:57:55 -0400

    Hello

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        String x;
+        System.out.println(x: "Hello World");
+        
+    }
+
+}
